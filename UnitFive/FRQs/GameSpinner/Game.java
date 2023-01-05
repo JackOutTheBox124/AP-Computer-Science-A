c1b48ad0edcfa61ad352bf53aec7e0a909735d8a
Jack <jack@jackoutthebox.dev>
2023-01-05 08:05:10 -0500

    Finished GameSpinner FRQ

--- UnitFive/FRQs/GameSpinner/Game.java
+++ UnitFive/FRQs/GameSpinner/Game.java
@@ -1,163 +1,217 @@
 package UnitFive.FRQs.GameSpinner;
 
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * This class represents a game that has x options:
+ * a slot machine or a double spinner.
+ * <br>
+ * The slot machine allows the player to spin a set of three {@link GameSpinner} objects and win credits if the numbers on all three spinners match and they land on the highest number possible (5 or whatever number the user enters).
+ * The double spinner allows the player to spin two {@link GameSpinner} objects and win credits if the numbers on the two spinners match.
+ */
 public class Game {
+
+  /**
+   * The main method of the Game class which allows the player to select which game
+   * they want to play and set the number of sectors for each {@link GameSpinner} object
+   */
   public static void main(String[] args) {
-    int coins = 0;
+
+    // Create a new Scanner object for user input
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("What game would you like to play?\n" +
-            "(1) - Slot Machine (gambling is good (real))\n" +
-            "(2) - ");
+    // Prompt the user to select a game
+    System.out.println("""
+            What game would you like to play?
+            (1) - Slot Machine     (gambling is good (real))
+            (2) - Double Spinner   (gambling is bad  (fake))
+            (3) - Streak Spinner
+            """);
+    // Store the user's selection in the game variable
+    int game = scan.nextInt();
 
-    System.out.println("How many sectors should each spinner have?");
+    // Prompt the user to enter the number of sectors for each spinner
+    System.out.println("How many sectors should each spinner have? Any number less than 3 will be set to 3.");
+    // Store the user's input in the sectors variable
     int sectors = scan.nextInt();
 
-    while (sectors <= 0) {
-      System.out.println("Please enter a number greater than 0!");
-      sectors = scan.nextInt();
-    }
-
-
-
+    // Create an array of three GameSpinner objects with the specified number of sectors
     GameSpinner[] spinners = {
-      new GameSpinner(sectors),
-      new GameSpinner(sectors),
-      new GameSpinner(sectors),
+            new GameSpinner(sectors),
+            new GameSpinner(sectors),
+            new GameSpinner(sectors),
     };
 
+    // Select the game based on the user's input
+    switch (game) {
+      case 1 ->
+        // If the user selected the slot machine game, call the slotMachine method
+              slotMachine(spinners, sectors);
+      case 2 ->
+        // If the user selected the double spinner game, call the doubleSpinner method
+              doubleSpinner(spinners);
+      case 3 ->
+        // If the user selected the streak spinner game, call the streakSpinner method
+              streakSpinner(spinners);
+      default -> System.out.println("Goodbye!");
+    }
 
+  }
 
-    for (int i = 0; i < 20; i++) {
-      for (GameSpinner spinner:spinners) {
-        while (spinner.currentRun() <= 5) {
-          spinner.spin();
+  /**
+   * This method represents a game of chance where the player can spin three {@link GameSpinner}
+   * objects and win credits if the numbers on all three spinners match.
+   *
+   * @param spinners an array of three {@link GameSpinner} objects
+   */
+  public static void slotMachine(GameSpinner[] spinners, int sectors) {
+    // Create a new Scanner object for user input
+    Scanner scanner = new Scanner(System.in);
+
+    // Set the number of credits to 100
+    int credits = 100;
+    // Continue the game as long as the player has credits remaining
+    while (credits > 0) {
+      // Prompt the user to spin the spinners
+      System.out.println("You have " + credits + " credits. Spin? (y/n)");
+      // Store the user's input in the input variable
+      String input = scanner.nextLine();
+
+      // If the user chose to spin, spin all three spinners and store the result in variables
+      if (input.equals("y")) {
+        // Spin the first spinner and store the result in num1
+        int num1 = spinners[0].spin();
+        // Spin the second spinner and store the result in num2
+        int num2 = spinners[1].spin();
+        // Spin the third spinner and store the result in num3
+        int num3 = spinners[2].spin();
+        // Print the result of the spin
+        System.out.println("You spun: " + num1 + " " + num2 + " " + num3);
+
+        // Check if all three numbers are the same
+        if (num1 == num2 && num2 == num3 && num1 == sectors) {
+          // If the numbers are the same and equal to the sector (the highest face value), the player wins 10 credits
+          System.out.println("You won 10 credits!");
+          credits += 10;
+        } else {
+          // If the numbers are not the same, the player loses 1 credit
+          System.out.println("You lost 1 credit.");
+          credits--;
         }
-        // spinner.spin();
-        System.out.println(spinner.currentRun());
+      } else {
+        // If the user did not choose to spin, end the game
+        break;
       }
     }
-
-
-  }
-
-
-  public static class symbols {
-    public static final int cherries =    1;
-    public static final int bells =       2;
-    public static final int watermelons = 3;
-    public static final int lemons =      4;
-    public static final int plums =       5;
-    public static final int oranges =     6;
-    public static final int grapes =      7;
-    public static final int ace =         8;
-    public static final int sevens =      9;
-    public static final int diamonds =   10;
-
-/*    String[] symbols = {
-      "cherries",
-              "bells",
-              "sevens",
-              "bars",
-              "watermelons",
-              "lemons",
-              "plums",
-              "oranges",
-              "grapes",
-              "diamonds",
-    };
-
- */
-  }
-  public int slotMachine() {
-
-
-    GameSpinner[] machineReels = {
-      new GameSpinner(10),
-      new GameSpinner(10),
-      new GameSpinner(10),
-    };
-
-    for (GameSpinner spinner:machineReels) {
-      spinner.spin();
+    // If the player has run out of credits, print a message
+    if (credits == 0) {
+      System.out.println("You ran out of credits!");
     }
+    // Print a goodbye message
+    System.out.println("Goodbye!");
+  }
 
-    if (machineReels[0].getNum() == machineReels[1].getNum() &&
-    machineReels[0].getNum() == machineReels[2].getNum()) {
+  /**
+   * Simulates a gambling game where the player spins two "spinners" and wins credits if the numbers on the spinners are the same, and loses credits if the numbers are different. The player can keep playing as long as they have credits remaining.
+   *
+   * @param spinners an array of GameSpinner objects representing the spinners in the game
+   */
+  public static void doubleSpinner(GameSpinner[] spinners) {
+    // Initialize the number of credits to 100
+    int credits = 100;
+    // Create a Scanner object to read input from the user
+    Scanner scanner = new Scanner(System.in);
 
-      switch (machineReels[0].getNum()) {
-        case symbols.cherries -> {
-          System.out.println("1 1 1");
-          return 10;
-        }
-        case symbols.bells -> {
-          System.out.println("2 2 2");
-          return 20;
-        }
-        case symbols.watermelons -> {
-          System.out.println("3 3 3");
-          return 30;
-        }
-        case symbols.lemons -> {
-          System.out.println("4 4 4");
-          return 40;
-        }
-        case symbols.plums -> {
-          System.out.println("5 5 5");
-          return 50;
-        }
-        case symbols.oranges -> {
-          System.out.println("6 6 6");
-          return 60;
-        }
-        case symbols.grapes -> {
-          System.out.println("7 7 7");
-          return 70;
-        }
-        case symbols.ace -> {
-          System.out.println("8 8 8");
-          return 80;
-        }
-        case symbols.sevens -> {
-          System.out.println("9 9 9");
-          return 90;
-        }
-        case symbols.diamonds -> {
-          System.out.println("10 10 10");
-
-          return 100;
+    // Loop as long as the player has credits remaining
+    while (credits > 0) {
+      // Prompt the user to spin the spinners
+      System.out.println("You have " + credits + " credits. Spin? (y/n)");
+      // Store the user's input in the input variable
+      String input = scanner.nextLine();
+      if (input.equals("y")) {
+        // Spin both spinners
+        spinners[0].spin();
+        spinners[1].spin();
+        // Print the numbers that were spun
+        System.out.println("You rolled a " + spinners[0].getNum() + " and a " + spinners[1].getNum());
+        if (GameSpinner.bothEqual(spinners[0], spinners[1])) {
+          // If the numbers are the same, the player wins 10 credits
+          System.out.println("You won 10 credits!");
+          credits += 10;
+        } else {
+          // If the numbers are not the same, the player loses 1 credit
+          System.out.println("You lost 1 credit.");
+          credits--;
         }
       }
+      // If the user input "n", break out of the loop
+      else {
+        break;
+      }
     }
-    else if(machineReels[1].getNum() == symbols.cherries &&
-            (machineReels[0].getNum() == symbols.cherries ||
-                    machineReels[2].getNum() == symbols.cherries)) {
-      System.out.println(machineReels[0].getNum() + " " + machineReels[1].getNum() + " " + machineReels[2].getNum());
-      return 5;
+    // If the player has run out of credits, print a message
+    if (credits == 0) {
+      System.out.println("You ran out of credits!");
     }
-
-       // else if ()
-
-
-    return -1;
+    // Print a goodbye message
+    System.out.println("Goodbye!");
   }
 
 
+  /**
+   * Simulates a gambling game where the player spins a GameSpinner object and wins credits if the number on the spinner is the same as the previous spin,
+   * and loses credits if the number is different.
+   * The player can keep playing as long as they have credits remaining.
+   * @param spinners an array of GameSpinner objects representing the spinners in the game
+   */
+  public static void streakSpinner(GameSpinner[] spinners) {
+    // Initialize the number of credits to 100
+    int credits = 100;
+    int runHighScore = 1;
+    // Create a Scanner object to read input from the user
+    Scanner scanner = new Scanner(System.in);
 
-  public static boolean moreThanOnce(ArrayList<Integer> arrayList, int searched)
-  {
-    int numCount = 0;
+    // Loop as long as the player has credits remaining
+    while (credits > 0) {
+      // Prompt the user to spin the spinners
+      System.out.println("You have " + credits + " credits. Spin? (y/n)");
+      // Store the user's input in the input variable
+      String input = scanner.nextLine();
+      if (input.equals("y")) {
+        // spins spinners twice so the loop will work
+        spinners[0].spin();
+        spinners[0].spin();
+        // loop to spin the game spinner and check the current run against the high score
+        // exits loop when the current run is broken
+        while(spinners[0].currentRun() > 1) {
+          if(spinners[0].currentRun() > runHighScore) runHighScore = spinners[0].currentRun();
+          spinners[0].spin();
+        }
+        // Print the numbers that were spun
+        System.out.println("Your streak was " + runHighScore);
+        // Awards the player for a streak and deducts credits for a loss
+        if(runHighScore > 1) {
+          // The user receives 10 credits for each number in the streak
+          System.out.println("You won " + (10 * runHighScore) + " credits!");
+          credits += 10 * runHighScore;
+        }
+        else {
+          // If the numbers are not the same, the player loses 10 credits
+          System.out.println("You lost 10 credits");
+          credits -= 10;
+        }
+        // resets the high score
+        runHighScore = 1;
+      }
+      else break;
 
-    for (int thisNum : arrayList) {
-      if (thisNum == searched) numCount++;
     }
+    // If the player has run out of credits, print a message
+    if (credits <= 0) {
+      System.out.println("You ran out of credits!");
+    }
+    // Print a goodbye message
+    System.out.println("Goodbye!");
 
-    return numCount > 1;
   }
-
 }
-
-
--- UnitFive/FRQs/GameSpinner/GameSpinner.java
+++ UnitFive/FRQs/GameSpinner/GameSpinner.java
@@ -1,31 +1,89 @@
 package UnitFive.FRQs.GameSpinner;
 
+/**
+ * The GameSpinner class represents a spinner with a certain number of sectors,
+ * which can be used to play games that involve spinning a spinner.
+ */
 public class GameSpinner {
-
+  /** The number of sectors on the spinner */
   private int sectors;
+
+  /** The current number of times in a row that the spinner has landed on the same number */
   private int currentRun = 0;
+
+  /** The number that the spinner landed on in the last spin */
   private int lastNum;
 
+  /**
+   * Constructs a new GameSpinner object with the specified number of sectors.
+   *
+   * @param sectors the number of sectors on the spinner
+   */
   public GameSpinner(int sectors) {
-    this.sectors = sectors;
+    this.sectors = Math.max(3, sectors);
   }
 
+  /**
+   * Spins the spinner and returns the number that it lands on.
+   *
+   * @return the number that the spinner landed on
+   */
   public int spin() {
+    // Generate a random number between 1 and the number of sectors
     int num = (int) (Math.random() * sectors) + 1;
+
+    // If the spinner landed on the same number as the last spin or this is the first spin,
+    // increment the current run count. Otherwise, reset the current run count to 1.
     if (num == lastNum || lastNum == 0) {
       currentRun++;
     } else {
       currentRun = 1;
     }
+
+    // Update the lastNum field to store the number that was just spun
     lastNum = num;
+
+    // Return the number that the spinner landed on
     return num;
   }
 
+  /**
+   * Returns the number that the spinner landed on in the last spin.
+   *
+   * @return the number that the spinner landed on in the last spin
+   */
   public int getNum() {
     return lastNum;
   }
 
+  /**
+   * Returns the current number of times in a row that the spinner has landed on the same number.
+   *
+   * @return the current number of times in a row that the spinner has landed on the same number
+   */
   public int currentRun() {
     return currentRun;
+  }
+
+  /**
+   * Calculates the probability of the spinner landing on a certain number, given the number of sectors on the spinner.
+   *
+   * @param sectors the number of sectors on the spinner
+   * @param num the number to calculate the probability for
+   * @return the probability of the spinner landing on the specified number
+   */
+  public static double probability(int sectors, int num) {
+    return (double) 1 / sectors;
+  }
+
+  /**
+   * Determines whether the spinner objects have landed on the same number in the last spin.
+   *
+   * @param spinner1 the first spinner object
+   * @param spinner2 the second spinner object
+   * @return true if both spinners have landed on the same number, false otherwise
+   */
+  public static boolean bothEqual(GameSpinner spinner1, GameSpinner spinner2) {
+    return spinner1.getNum() == spinner2.getNum();
   }
 }
